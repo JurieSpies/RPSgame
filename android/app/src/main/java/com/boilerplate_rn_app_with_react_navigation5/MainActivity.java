bd092d61332d541f8322a996b72831a5844bcde0
Jurie <jurie.spies@openvantage.co.za>
2020-07-26 21:33:20 +0200

    Start of RN app 0.63.2

---
+++ android/app/src/main/java/com/boilerplate_rn_app_with_react_navigation5/MainActivity.java
@@ -0,0 +1,15 @@
+package com.boilerplate_rn_app_with_react_navigation5;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "boilerplate_rn_app_with_react_navigation5";
+  }
+}
